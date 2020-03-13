c937e0be2dda71223f3120230a9b7e0587dc87a3
Anthony Burton <ab@anthbrtn.com>
2020-03-13 09:56:20 -0400

    combined scrapers to use selenium

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 The scripts expect a `.csv` file named `GFMURL.csv` in the directory.
 
 # Grabbing campaign info
-Run the `parser.py` script. It will grab campaign information and donation
-information from static pages.
+Run the `parser.py` script. It will grab campaign information, comment information, and donation
+information from scraped pages using selenium's webdriver, which automates
+loading and scraping the content of each page. This script used to be split into
+two separate scripts, but I've combined them for usability's sake.
 
-# Grabbing comments
-Since the comments are loaded dynamically, this script has been separated into
-the `selenium-downloader.py` script. Run this and make sure that you have
-Firefox installed, as well as have followed the instructions at [Pythonspot's
+# Using the selenium web automator
+Make sure that you have Firefox installed, as well as have followed the instructions at [Pythonspot's
 Selenium setup page](https://pythonspot.com/selenium/). Then run the script and
 don't worry if a Firefox window pops up and starts doing some stuff. 
 
-This script outputs the comments into `comments.csv`. It is currently only
+This script outputs the campaign information into `campaigns.csv`, the donation
+information into `donations.csv`, and the comments into `comments.csv`. It is currently only
 capable of grabbing ten comments per campaign.
--- parser.py
+++ parser.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import re
 import csv
 import requests
 import time
-import json
 import os
 
+# load our csvs for input and output
 urlreader = csv.reader(open('./GFMURL.csv', 'r'))
-htmlwriter = csv.writer(open('./all-pages.csv','w+'))
+htmlwriter = csv.writer(open('./campaigns.csv','w+'))
 donationwriter = csv.writer(open('./donations.csv','w+'))
+commentswriter = csv.writer(open('./comments.csv','w+'))
 exceptions = open('./exceptions.txt','w+')
-htmlwriter.writerow(['organizer', 'location', 'category', 'description', 'created', 'money_raised', 'goal'])
-donationwriter.writerow(['campaign','donation_id', 'amount', 'made_offline', 'is_anonymous', 'name', 'donation_date', 'profile', 'verified_user', 'comments'])
+commentswriter.writerow('campaign, name, donation, comment, url')
+htmlwriter.writerow('organizer, location, category, description, created, money_raised, goal, url')
+donationwriter.writerow(['campaign','donation_id', 'amount', 'made_offline', 'is_anonymous', 'name', 'donation_date', 'profile', 'verified_user', 'comments', 'url'])
+# Create our web driver to load and scrape the pages 
+options = Options()
+options.headless = True
+driver = webdriver.HtmlUnitDriver(options=options)
+driver.setJavascriptEnabled(true)
 
-def parse_html(soup):
-    try: 
+def parse_html(soup, url):
+    try:
         created_date = soup.find('span', {'class': 'm-campaign-byline-created'}).get_text().replace('Created ', '')
         organizer_block = soup.find('div', {'class': 'm-campaign-members-main'})
         name = organizer_block.find('div', {'class': 'm-person-info-name'}).get_text()
@@ -27,14 +34,14 @@ def parse_html(soup):
         except:
             goal = soup.find('h2', {'class':'m-progress-meter-heading'}).get_text()
             sofar = '0'
-        pagerow = [name, location, category, description, created_date, sofar, goal]
+        pagerow = [name, location, category, description, created_date, sofar, goal, url]
         # comment_row = find_comments(soup)
         # print(comment_row)
         htmlwriter.writerow(pagerow)
     except:
         exceptions.write(row[0]+" was unable to be parsed\n")
-    
-def find_donations(soup):
+
+def find_donations(soup, url):
     # json.loads and ast.literal_eval don't work here, need to split manually
     # this is really, really messy code becaue of the peculiarities of parsing json as a string
     try:
@@ -61,25 +68,58 @@ def find_donations(soup):
                 field = field.split('":')[1]
                 field.replace('"', '').replace("'", '')
                 donation_row_constructor.append(field)
+            donation_row_constructor.append(url)
             donationwriter.writerow(donation_row_constructor)
             donation_row_constructor = []
     except:
         exceptions.write("no donations")
-        
+
+
 def find_comments(soup):
+    title = soup.title.get_text()
     try:
         comment_info = []
-        comment_soup = soup.find_all('div', {'class': 'm-comment'})
-        for comment in comment_soup:
-            comment_info.append(comment.get_text())
-        return comment_info
+        comment_scrip = []
+        row = []
+        row.append(title)
+        comment_head = soup.find_all('div', {'class': 'm-comment-description'})
+        comment_description = soup.find_all('div', {'class': 'm-comment-content'})
+        for i in comment_head:
+            body = i.get_text()
+            comment_info.append(body)
+            # more fine-graining is available, but unfinished
+            # if '$' in body:
+            #     don_split = body.split(' donated ')
+            #     for d in don_split:
+            #         comment_info.append(d)
+            # elif :
+            #     don_split = body.split(' commented')
+            #     don_split[1] = 0
+            #     for d in don_split:
+            #         comment_info.append(d)
+        for k in comment_description:
+            comment_scrip.append(k.get_text())
+        for i in range(len(comment_info)):
+            row.append(comment_info[i])
+            row.append(comment_scrip[i])
+            row.append(url)
+            commentswriter.writerow(row)
+            row = []
+            row.append(title)
     except:
-        exceptions.write("no comments")
+        exceptions.write("no comments for")
 
-for row in urlreader:
+for i in urlreader:
+    driver.get(i)
+    driver.execute_script("window.scrollBy(0,5000)")
     time.sleep(1)
-    response = requests.get(row[0], headers = {'User-agent': 'webscraper'})
-    soup = BeautifulSoup(response.text)
-    parse_html(soup)
-    find_donations(soup)
-    print(soup.title.get_text()+" has been parsed.")
+    driver.execute_script("window.scrollBy(0,5000)")
+    time.sleep(1)
+    driver.execute_script("window.scrollBy(0,5000)")
+    time.sleep(1)
+    driver.execute_script("window.scrollBy(0,5000)")
+    soup = BeautifulSoup(driver.page_source, "html5lib")
+    parse_html(soup, i)
+    find_donations(soup, i)
+    find_comments(soup, i)
+    print(soup.title.get_text()+" scraped for comments")
