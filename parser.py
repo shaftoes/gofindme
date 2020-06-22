92183da83e53580071e33ce49c650c40553f6878
Anthony Burton <ab@anthbrtn.com>
2020-06-22 11:19:21 -0400

    Split tables for search results and full ones

--- parser.py
+++ parser.py
@@ -8,6 +8,7 @@ import os
 from selenium import webdriver
 import sys
 import sqlite3
+import datetime
 from multiprocessing import Process
 
 # basic usage: call the script with "search term" as an argument
@@ -16,8 +17,7 @@ from multiprocessing import Process
 # this is set up to be changed; empty items 1 and 3 are changed per requests call
 api_string = [
     'https://www.gofundme.com/mvc.php?route=homepage_norma/load_more&page=',
-    '', '&term=', '', '&country=&postalCode=&locationText='
-]
+    '', '&term=', '']
 
 
 def generate_api_call(search_term, page_number):
@@ -33,7 +33,14 @@ def initialize_db(database_name):
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS urls(
-    url text PRIMARY KEY
+    url text PRIMARY KEY,
+    image text,
+    title text,
+    location text,
+    description text,
+    raised text,
+    goal text,
+    deleted text
     )""")
 
     cursor.execute("""
@@ -72,46 +79,19 @@ def initialize_db(database_name):
     name text,
     donation_date text,
     profile text,
-    verifie_user integer,
+    verified_user integer,
     comments text
+    )""")
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS meta(
+    date text PRIMARY KEY,
+    query text,
+    num_results text
     )""")
     database_connection.commit()
     return cursor, database_connection
-
-
-def grab_urls_selenium(search_term, driver, database_cursor,
-                       database_connection):
-    url_list = []
-    main_page = requests.get(
-        'https://www.gofundme.com/mvc.php?route=homepage_norma/search&term=' +
-        search_term).text
-    main_soup = BeautifulSoup(main_page, "html5lib")
-    number_of_results = main_soup.find("div", {
-        'class': 'heading-3'
-    }).get_text().replace(' results found', '')
-    # we can get our page number by dividing by 9 - the amount of posts on a page
-    number_of_pages = int(number_of_results) // 9
-    driver.get(
-        'https://www.gofundme.com/mvc.php?route=homepage_norma/search&term=' +
-        search_term)
-    for i in range(1, number_of_pages):
-        time.sleep(1)
-        # for elem in driver.find_elements_by_link_text('Show more'):
-        for elem in driver.find_elements_by_class_name("js-show-more-results"):
-            try:
-                elem.click()
-                print('Succesful click')
-            except:
-                print('Unsuccesful click')
-                break
-    soup = BeautifulSoup(driver.page_source, "html5lib")
-    campaigntiles = soup.find_all("a", {'class': 'campaign-tile-img--contain'})
-    for c in campaigntiles:
-        url = c.get('href')
-        database_cursor.execute(
-            """INSERT OR IGNORE INTO urls(url) VALUES (?)""", [url])
-        database_connection.commit()
-    print('Done a search pass for ' + search_term)
 
 
 def grab_urls_php(search_term, database_cursor, database_connection):
@@ -123,45 +103,64 @@ def grab_urls_php(search_term, database_cursor, database_connection):
     number_of_results = main_soup.find("div", {
         'class': 'heading-3'
     }).get_text().replace(' results found', '')
+    database_cursor.execute(
+        """INSERT INTO meta(date, query, num_results) VALUES (?, ?, ?)""",
+        [datetime.datetime.now(), search_term, number_of_results])
     # we can get our page number by dividing by 9 - the amount of posts on a page
     number_of_pages = int(number_of_results) // 9
-    # need to call at range one because our api_call asks for a page number, which is invalid for 0
+    # need to call at range one because our api_call asks for a page number,
+    # which is invalid for 0
     for i in range(1, number_of_pages):
         time.sleep(1)
         api_call = generate_api_call(search_term, i)
         # call the php api and parse it
         r = requests.get(api_call)
         soup = BeautifulSoup(r.text, "html5lib")
-        campaigntiles = soup.find_all("a",
-                                      {'class': 'campaign-tile-img--contain'})
-        # print("Requested page "+str(i)+' and received code '+str(r.status_code)+'. There are '+str(len(campaigntiles))+" on page "+str(i))
+        campaigntiles = soup.find_all("div",
+                                      {'class': 'react-campaign-tile'})
         if len(campaigntiles) == 0:
             print("Search stopped at page " + str(i))
             break
         for c in campaigntiles:
-            url = c.get('href')
-            print(url)
+            url = (c.find("a")['href'])
+            info = parse_tile(c)
+            info.insert(0,url)
             database_cursor.execute(
-                """INSERT OR IGNORE INTO urls(url) VALUES (?)""", [url])
+                """INSERT OR IGNORE INTO urls(
+                url, image, title, location, description, raised, goal, deleted)
+                VALUES (?,?,?,?,?,?,?,?)""", info)
             database_connection.commit()
     print('Done a search pass for ' + search_term)
 
 
 # second set of functionaities: scraping the pages themselves
 
+def parse_tile(campaign_tile):
+    image = campaign_tile.find("a", {"class":"campaign-tile-img--contain"})['data-original']
+    infotile = campaign_tile.find("div",{"class":"react-campaign-tile-details"})
+    title = infotile.find("div", {"class":"fund-title"}).get_text()
+    location = infotile.find("div",{"class":"fund-location"}).get_text().strip()
+    description = infotile.find("div",{"class":"fund-description"}).get_text().strip()
+    fundblock = infotile.find("div",{"class":"fund-item truncate-single-line"}
+                                 ).get_text().strip().split("of")[0:2]
+    raised = fundblock[0].rstrip(" raised")
+    goal = fundblock[1].split(" ")[1]
+    deleted = check_deleted(campaign_tile.find("a", {"class":"campaign-tile-img--contain"})['href'])
+    return [image,title,location,description,raised,goal,deleted]
 
-def load_csv(csv, database_cursor, database_connection):
-    urlreader = csv.reader(open(csv, 'r'))
-    for row in urlreader:
-        database_cursor.execute(
-            """
-        INSERT OR IGNORE INTO campaigns(url) VALUES (?)""", row)
-    database_cursor.commit()
-
+def check_deleted(url):
+    if BeautifulSoup(requests.get(url).text,"html5lib").title.get_text() == "Page Not Found":
+        return datetime.datetime.now()
+    else:
+        return "No"
 
 def find_campaign_info(soup, url):
-    try:
-        name = soup.title.get_text()
+    name = soup.title.get_text()
+    if name == "Page Not Found":
+        deletion_date = datetime.datetime.now()
+        return
+    else:
+        deletion_date = "exists"
         created_date = soup.find('span', {
             'class': 'm-campaign-byline-created'
         }).get_text().replace('Created ', '')
@@ -225,23 +224,21 @@ def find_campaign_info(soup, url):
             }).get_text()
             sofar = '0'
         # write all of these values to a row to be written to the csv
-        pagerow = [
+    pagerow = [
             url, name, organizer, location, category, description,
-            created_date, sofar, goal, donors, shares, followers
+            created_date, sofar, goal, donors, shares, followers, deletion_date
         ]
-        return pagerow
-    except:
-        # catching our exceptions onto the command line
-        print(url + " was unable to be parsed\n")
+    return pagerow
 
 
 def find_donations(soup, url):
-    # this finds all donation-related information and scrapes it into donations.csv
+    """find all donation-related information and scrapes it into donations.csv"""
     campaign_title = soup.title.get_text()
     donation_array = []
-    # json.loads and ast.literal_eval don't work here, so we need to split manually (pythonically?)
-    # this is really, really messy code becaue of the peculiarities of parsing json as a string. If this section throws errors, then the code is out-of-date --
-    # probably due to a redesign of the website.
+    # json.loads and ast.literal_eval don't work here, so we need to split manually
+    # this is really, really messy code becaue of the peculiarities of parsing 
+    # json as a string. If this section throws errors, then the code is
+    # out-of-date -- probably due to a redesign of the website.
     try:
         jsonstring = str(soup.find_all('script')[0])
         donation_row_constructor = []
@@ -281,8 +278,7 @@ def find_comments(soup, url):
     try:
         comment_info = []
         comment_scrip = []
-        row = []
-        row.append(title)
+        row = [title]
         comment_head = soup.find_all('div', {'class': 'm-comment-description'})
         comment_description = soup.find_all('div',
                                             {'class': 'm-comment-content'})
@@ -299,22 +295,27 @@ def find_comments(soup, url):
             comment_array.append(row)
             row = []
             row.append(title)
+        print("comments grabbed for" + title)
+        print(comment_array)
         return comment_array
     except:
+        print("no comments for" + title)
         return []
-        print("no comments for")
 
 
-def load_and_parse(database_cursor, database_connection):
+def load_and_parse(database_cursor, database_connection, key):
     # main wrapper function for the scraper functions - loads the page and calls
     # the scrapers
-    urls = database_cursor.execute("""
-    SELECT * FROM urls WHERE NOT EXISTS (
-    SELECT NULL
-    FROM campaigns
-    WHERE urls.url = campaigns.url
-    )""").fetchall()
-
+    if key == "update":
+        urls = database_cursor.execute("""
+        SELECT * FROM urls WHERE NOT EXISTS (
+        SELECT NULL
+        FROM campaigns
+        WHERE urls.url = campaigns.url
+        )""").fetchall()
+    else:
+        urls = database_cursor.execute("""
+        SELECT * FROM urls""").fetchall()
     for url_tuple in urls:
         url = url_tuple[0]
         driver.get(url)
@@ -323,64 +324,56 @@ def load_and_parse(database_cursor, database_connection):
             # more if we reach the bottom - more elements are hidden behind javascript calls
             driver.execute_script("window.scrollBy(0,5000)")
             time.sleep(0.2)  # sleep to let the browser catch up
-        # for x in range(0,20):
-        # # we try clicking the 'show comments' button 20 times.
-        # # the choice of 20 is also arbitrary, but this would load 100 comments
-        #     try:
-        #         # this click is not working right now
-        #         morecomments = driver.find_element_by_x_path("//a[@data-element-id='btn_donate_morecomments']")
-        #         morecomments.click()
-        #         time.sleep(1)
-        #     except:
-        #         break
-        # for x in range(0,20):
-        #     # attempt to click the "show more" to see more donations
-        #     try:
-        #         # this click is not working right now
-        #         moredonations = driver.find_element_by_x_path("//a[@data-element-id='btn_donate_moredonations']")
-        #         moredonations.click()
-        #     except:
-        #         break
-        # load the page's source as a soup object *after* we're done clicking and loading
-        # dynamic javascript elements
         soup = BeautifulSoup(driver.page_source, "html5lib")
-        # juSt to make sure that the page exists and we're not wasting our time
         try:
+            campaign_info = find_campaign_info(soup, url)
             database_cursor.execute(
-                """INSERT OR IGNORE INTO campaigns(
-            url, name, organizer, location, category, description, created, money_raised, goal, donors, shares, followers
-            ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)""",
-                find_campaign_info(soup, url))
-            for comment in find_comments(soup, url):
-                database_cursor.execute(
-                    """INSERT OR IGNORE INTO comments(campaign, url, name, donation, comment) VALUES (?,?,?,?,?,?,?,?)""",
-                    comment)
-            for donation in find_donations(soup, url):
-                database_cursor.execute(
-                    """
-                INSERT OR IGNORE INTO donations(donation_id, url, campaign, amount, made_offline, is_anonymous, name, donation_date, profile, verified_user, comments) VALUES (?,?,?,?,?,?,?,?,?,?,?)""",
-                    find_donations(soup, url))
-            database_connection.commit()
-            continue
+                """INSERT OR REPLACE INTO campaigns(
+            url, name, organizer, location, category, description, created, money_raised, goal, donors, shares, followers, does_exist
+            ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?, ?)""",
+                campaign_info)
         except:
             print(
                 "The campaign at " + url +
                 " could not be found. It's either been deleted, or the url is incorrect.\n"
             )
             continue
+        try:
+            for donation in find_donations(soup, url):
+                database_cursor.execute(
+                    """
+                INSERT OR IGNORE INTO donations(
+                    donation_id, url, campaign, amount, made_offline, is_anonymous, name, donation_date, profile, verified_user, comments)
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?)""", find_donations(soup, url))
+            database_connection.commit()
+        except:
+            print(
+                "Couldn't find donations for the campaign at " + url
+            )
+            continue
+        try:
+            for comment in find_comments(soup, url):
+                database_cursor.execute(
+                    """INSERT OR IGNORE INTO comments(campaign, url, name, donation, comment)
+                    VALUES (?,?,?,?,?,?,?,?)""", comment)
+        except:
+            print(
+                "Couldn't find comments for the campaign at " + url
+            )
+            continue
 
 
 if __name__ == '__main__':
-    query = sys.argv[1]
-    passes = int(sys.argv[2])
+    if sys.argv[2]:
+        passes = int(sys.argv[2])
+    else:
+        passes = 1
     cursor, connection = initialize_db(sys.argv[1])
     for x in range(0, passes):
-        # try:
-        grab_urls_php(query, cursor, connection)
+        grab_urls_php(sys.argv[1], cursor, connection)
         print('success')
-        # except:
-        # pass
     driver = webdriver.Firefox()
-    load_and_parse(cursor, connection)
+    key = sys.argv[3]
+    load_and_parse(cursor, connection, key)
     connection.commit()
     connection.close()
